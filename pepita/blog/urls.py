2b48dd9ab7238cd882ee3ff926ba8465e95e2754
christopher <christopher.gizard@etu.univ-lyon1.fr>
2020-10-12 17:01:11 +0200

    DeleteView + ListePosteByTag + pagination

--- pepita/blog/urls.py
+++ pepita/blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.ListePosts.as_view(), name="liste_post"),
+	path('byTag/<tag>', views.ListePostsByTag.as_view(), name="liste_post_by_tag"),
 	path('creer-post', views.CreationPost.as_view(), name="creer_post"),
+	path('modifier-post/<int:id>/<slug:slug>', views.ModificationPost.as_view(), name="modifier_post"),
+	path('supprimer_post/<int:id>/<slug:slug>', views.SuppressionPost.as_view(), name="supprimer_post"),
 
 
 ]
--- pepita/blog/views.py
+++ pepita/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
@@ -6,7 +6,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView, D
 from .models import Post
 from .forms import PostForm
 
-#Affiche la liste des billets du post
+#Affiche la liste des billets du blog
 class ListePosts(ListView):
 	model = Post
 
@@ -19,7 +19,31 @@ class ListePosts(ListView):
 	def get_context_data(self, *args, **kwargs):
 		# Call the base implementation first to get a contexte
 		context = super().get_context_data(*args)
-		# On recupere la categorie de la page
+		# On recupere les 20 tags les plus communs
+		tags = Post.tags.most_common(1)[:20]
+		print(tags)
+		context["tags"] = tags
+		return context
+
+#Affiche la liste des billets du blog
+class ListePostsByTag(ListView):
+	model = Post
+
+	context_object_name = "posts"
+
+	template_name = "blog/liste.html"
+
+	paginate_by = 10
+
+	def get_queryset(self):
+		if 'tag' in self.kwargs:
+			tags = [self.kwargs['tag']]
+			return Post.objects.filter(tags__name__in=tags)
+
+	def get_context_data(self, *args, **kwargs):
+		# Call the base implementation first to get a contexte
+		context = super().get_context_data(*args)
+		# On recupere les 20 tags les plus communs
 		tags = Post.tags.most_common(1)[:20]
 		print(tags)
 		context["tags"] = tags
@@ -54,8 +78,31 @@ class ModificationPost(UpdateView):
 
 	template_name = 'blog/creer_post.html'
 
-	#form_class = 
+	form_class = PostForm
 
-	#success_url = reverse_lazy()
+	success_url = reverse_lazy('liste_post')
+
+	def get_object(self):
+		code = self.kwargs.get('id',None)
+		slug = self.kwargs.get('slug', None)
+		return get_object_or_404(Post,pk=code)
+
+	def form_valid(self, form):
+		form.instance.slug = form.cleaned_data['slug']
+		return super(CreationPost, self).form_valid(form)
+
+#Suppression d'un post
+class SuppressionPost(DeleteView):
+	model = Post
+
+	template_name = "blog/suppression_post.html"
+
+	success_url = reverse_lazy('liste_post')
+
+	def get_object(self):
+		code = self.kwargs.get('id',None)
+		slug = self.kwargs.get('slug', None)
+		return get_object_or_404(Post,pk=code)
+
 
 
